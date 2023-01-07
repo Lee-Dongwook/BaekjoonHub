5d1caff0a45ef4187966138c37c4dad7dfab73af
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-01-08 07:22:42 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 2276 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+int N, sum;
+int num1[10001], num2[10001];
+string s1, s2, tmp;
+vector<int> ans;
+
+int main()
+{
+	cin >> s1 >> s2;
+
+	
+	if (s1.size() < s2.size())
+	{
+		tmp = s1;
+		s1 = s2;
+		s2 = tmp;
+	}
+	
+	
+	for (int i = 0; i < s1.size(); i++)
+		num1[i + 1] = s1[i] - '0';
+
+	for (int i = 0; i < s2.size(); i++)
+		num2[i + 1 + (s1.size()-s2.size())] = s2[i] - '0';
+
+	
+	for (int i = s1.size(); i > 0; i--)
+	{
+		sum = num1[i] + num2[i];
+		if (sum >= 10)
+		{
+			num1[i - 1]++;
+			sum -= 10;
+		}
+		ans.push_back(sum);
+	}
+
+	
+	if (num1[0] != 0) cout << 1;
+
+	
+	for (int i = ans.size() - 1; i >= 0; i--)
+	{
+		cout << ans[i];
+	}
+
+}
