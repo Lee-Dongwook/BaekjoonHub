79c76784a43a938206dac64fa4dad728f37963f7
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-01-14 18:36:26 +0900

    [Silver II] Title: N번째 큰 수, Time: 460 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+ 
+int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+ 
+    int n;
+    cin >> n;
+    priority_queue<int, vector<int>, greater<int> > pq;
+    
+    for (int i = 0; i < n; i++) {
+        int data;
+        cin >> data;
+        pq.push(data);
+    }
+ 
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int data;
+            cin >> data;
+            pq.push(data);
+        }
+        for (int i = 0; i < n; i++)pq.pop();
+    }
+    cout << pq.top();
+    return 0;
+}
