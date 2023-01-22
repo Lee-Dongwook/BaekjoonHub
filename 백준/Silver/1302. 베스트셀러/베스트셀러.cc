895efe0c3db4fd758cb80c8a98229a9c5ef47515
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-01-23 00:20:00 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+using namespace std;
+map <string,int> m;
+int n, cnt ;
+int main(){
+    cin >> n;
+    while(n--){
+        string bookName;
+        cin >> bookName;
+        m[bookName]++;
+    }
+    for(auto p : m) cnt = max(cnt, p.second);
+    for(auto p : m){
+        if(p.second == cnt){
+            cout << p.first; 
+            return 0;
+        }
+    }
+    
+}
