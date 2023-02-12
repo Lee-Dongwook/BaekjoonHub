628211fe1ecbfccf49cde1159704ba2b621b111c
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-02-12 13:19:02 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+    
+    if(angle >0 && angle < 90)
+    {
+        answer = 1;
+    }
+    else if (angle == 90)
+    {
+        answer = 2;
+    }
+    else if (angle >90 && angle < 180)
+    {
+        answer = 3;
+    }
+    else if (angle == 180)
+    {
+        answer = 4;
+    }
+    
+    return answer;
+}
+
+int main(void)
+{
+    int angle;
+    cin>>angle;
+    
+    int result = solution(angle);
+    cout<<result;
+    
+    return 0;
+}
