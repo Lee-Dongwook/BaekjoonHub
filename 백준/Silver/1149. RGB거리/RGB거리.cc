d8dfa9de4cdd18ce5f42a475bf5ef9c4354abdac
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-05-06 21:46:33 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -2,51 +2,53 @@
 #include <algorithm>
 using namespace std;
 
+int N;
+int House[1001][3];
 int DP[1001][3];
-int num[1001][3];
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(0);
+	cout.tie(0);
 
-	int N;
 	cin >> N;
+	House[0][0] = 0;
+	House[0][1] = 0;
+	House[0][2] = 0;
+	DP[0][0] = 0;
+	DP[0][1] = 0;
+	DP[0][2] = 0;
 
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cin >> num[i][j];
+			int num;
+			cin >> num;
+			House[i][j] = num;
 		}
 	}
-	num[0][0] = 0;
-	num[0][1] = 0;
-	num[0][2] = 0;
 
-	DP[0][0] = 0;
-	DP[0][1] = 0;
-	DP[0][2] = 0;
+	//집이 1개
+	DP[1][0] = House[1][0];
+	DP[1][1] = House[1][1];
+	DP[1][2] = House[1][2];
 
-	DP[1][0] = 0;
-	DP[1][1] = 0;
-	DP[1][2] = 0;
-
-	DP[2][0] = min(num[1][1] + num[2][0], num[1][2] + num[2][0]);
-	DP[2][1] = min(num[1][0] + num[2][1], num[1][2] + num[2][1]);
-	DP[2][2] = min(num[1][0] + num[2][2], num[1][1] + num[2][2]);
+	//집이 2개
+	DP[2][0] = min(DP[1][1] + House[2][0], DP[1][2] + House[2][0]);
+	DP[2][1] = min(DP[1][0] + House[2][1], DP[1][2] + House[2][1]);
+	DP[2][2] = min(DP[1][0] + House[2][2], DP[1][1] + House[2][2]);
 
 	for (int i = 3; i <= N; i++)
 	{
-		DP[i][0] = min(DP[i-1][1] + num[i][0], DP[i-1][2] + num[i][0]);
-		DP[i][1] = min(DP[i-1][0] + num[i][1], DP[i-1][2] + num[i][1]);
-		DP[i][2] = min(DP[i-1][0] + num[i][2], DP[i-1][1] + num[i][2]);
+		DP[i][0] = min(DP[i-1][1] + House[i][0], DP[i-1][2] + House[i][0]);
+		DP[i][1] = min(DP[i-1][0] + House[i][1], DP[i-1][2] + House[i][1]);
+		DP[i][2] = min(DP[i-1][0] + House[i][2], DP[i-1][1] + House[i][2]);
 	}
 
 	int result = min(DP[N][0], min(DP[N][1], DP[N][2]));
-
 	cout << result;
-
+		
 	return 0;
 }
