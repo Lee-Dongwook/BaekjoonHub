bc2995f349b0db35805c624a6e0b8734150b1565
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-06-19 07:42:05 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -3,37 +3,51 @@
 #include <stack>
 using namespace std;
 
-int main(void) {
-	int k;
-	cin >> k;
-	
-	while (k > 0) {
-		k--;
-		string input;
-		cin >> input;
+stack<char> st;
 
-		stack<char> st;
-		string answer = "YES";
-		for (int i = 0; i < input.length(); i++) {
-			//'('는 스택에 저장
-			if (input[i] == '(') {
-				st.push(input[i]);
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		string ans;
+		cin >> ans;
+
+		int a = 0;
+		int b = 0;
+
+		for (int i = 0; i < ans.size(); i++)
+		{
+			if (ans[i] == '(')
+			{
+				a++;
+				st.push(ans[i]);
 			}
-			//')'가 나오면 스택에 저장된 '('와 짝이 맞는지 확인하고 pop
-			else if (!st.empty() && input[i] == ')' && st.top() == '(') {
-				st.pop();
-			}
-			//위의 경우를 제외하고는 모두 vps가 아닌 상황이므로 종료(break)
-			else {
-				answer = "NO";
-				break;
+			else if (ans[i] == ')')
+			{
+				b++;
+				if (st.empty())
+				{
+					break;
+				}
+				else {
+					st.pop();
+				}
 			}
 		}
-		//'('와 ')'가 짝이 안 맞는 경우 '('가 모두 pop되지 않기 때문에
-		// 스택이 비지 않는다. 따라서 이 경우는 vps가 아니므로 "NO"
-		if (!st.empty()) answer = "NO";
 
-		cout << answer << endl;
+		if (a == b) { cout << "YES" << "\n"; }
+		else if (a != b) { cout << "NO" << "\n"; }
+		
+		while (!st.empty()) { st.pop();}
+
 	}
+	
 	return 0;
 }
