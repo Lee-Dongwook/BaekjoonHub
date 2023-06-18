35f9f3f39b4b2d5884322e79303ea7fcee9a7fcc
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-06-18 15:53:50 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -2,32 +2,51 @@
 #include <queue>
 using namespace std;
 
-int N, K;
 queue<int> q;
+queue<int> res;
 
 int main(void)
 {
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++)
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
 	{
 		q.push(i);
 	}
-	cout << "<";
 
-	while (q.size() != 0)
+	while (!q.empty())
 	{
-		for(int i=1; i<K;i++){
-		q.push(q.front());
+		for (int i = 0; i < k - 1; i++)
+		{
+			int t = q.front();
+			q.pop();
+			q.push(t);
+		}
+
+		int t = q.front();
+		res.push(t);
 		q.pop();
 	}
-	cout << q.front();
-	if (q.size() != 1)cout << ", ";
-	q.pop();
+
+	cout << "<";
+	while (!res.empty())
+	{
+		if (res.size() > 1) {
+			cout << res.front() << ", ";
+			res.pop();
+		}
+		else if (res.size() == 1)
+		{
+			cout << res.front();
+			res.pop();
+		}
 	}
-
 	cout << ">";
-
-
 	return 0;
 
 }
