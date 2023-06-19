8e58547dd01e4d076f59fe012eef8e7ba68773dc
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-06-20 02:24:10 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string name;
+    cin>>name;
+    
+    string result = name + "??!";
+    cout<<result;
+    return 0;
+}
