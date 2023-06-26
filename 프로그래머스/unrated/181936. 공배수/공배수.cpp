f9058ed00a41c842b788ced9dbc034f5f4115a64
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-06-27 03:12:00 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    
+    if((number % n == 0) && (number % m == 0))
+    {
+        answer = 1;
+    }
+    else {
+        answer = 0;
+    }
+    
+    return answer;
+}
