a398be2a00f49d70a12049aa80fb05956b7306c7
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-06-27 03:21:16 +0900

    [unrated] Title: 홀짝 구분하기, Time: 2.23 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    
+    if(n % 2 == 0)
+    {
+        cout<<n<<" is even";
+    }
+    else if(n %2 != 0)
+    {
+        cout<<n<<" is odd";
+    }
+    
+    return 0;
+}
