9e7c89b6ed6be7706f3e14de07a7a2e0601552ef
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-02 23:20:31 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int score;
+    cin>>score;
+    
+    if(score >= 90 && score <= 100)
+    {
+        cout<<"A";
+    }
+    else if(score >= 80 && score < 90)
+    {
+        cout<<"B";
+    }
+    else if(score >= 70 && score < 80)
+    {
+        cout<<"C";
+    }
+    else if(score >= 60 && score < 70)
+    {
+        cout << "D";
+    }
+    else {
+        cout<<"F";
+    }
+    
+    return 0;
+}
