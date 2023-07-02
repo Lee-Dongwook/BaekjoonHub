6708e36f32d702a61ef0f6968acdb748c28f44d4
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-02 23:16:57 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int A,B;
+    cin>>A>>B;
+    
+    if(A < B)
+    {
+        cout<<"<";
+    }
+    else if(A > B)
+    {
+        cout<<">";
+    }
+    else if(A == B)
+    {
+        cout<<"==";
+    }
+    return 0;
+}
