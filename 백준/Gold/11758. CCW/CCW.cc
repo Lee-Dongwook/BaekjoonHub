0e9151fce44fa1ff5d2531a5c2480f14e73d450a
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-03 00:08:37 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.cc
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -3,15 +3,24 @@ using namespace std;
 
 int main(void)
 {
-    int ans;
-    int x1,x2,x3,y1,y2,y3;
-    cin>>x1>>y1>>x2>>y2>>x3>>y3;
-    int result;
-    result = (x1*y2 + x2*y3 + x3*y1) - (x2*y1+x3*y2+x1*y3);
+    int a1,b1,a2,b2,a3,b3;
+    cin>>a1>>b1>>a2>>b2>>a3>>b3;
     
-    if(result >0)ans = 1;
-    else if(result <0)ans = -1;
-    else ans = 0;
+    int res = (a1 * b2 + a2 * b3 + a3 * b1) - (a2 * b1 + a3 * b2 + a1 * b3);
+    
+    int ans = 0;
+    
+    if(res > 0)
+    {
+       ans = 1;
+    }
+    else if(res < 0)
+    {
+        ans = -1;
+    }
+    else{
+        ans = 0;
+    }
     
     cout<<ans;
     return 0;
