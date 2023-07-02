817e71c3c0ea1186a830e55cc687e4f654d9e809
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-02 15:36:23 +0900

    [Silver III] Title: 소수 구하기, Time: 436 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,38 +1,38 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-#define MAX 1000000
-
-int arr[MAX + 1]{ 0, };
-
-int main()
-{
-    int m, n;
-    cin >> m >> n;
-
-    for (int i = 2; i <= n; i++)
-    {
-        arr[i] = i;
-    }
-
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (arr[i] == 0)
-        {
-            continue;
-        }
-        for (int j = i * i; j <= n; j += i)
-        {
-            arr[j] = 0;
-        }
-    }
-
-    for (int i = m; i <= n; i++)
-    {
-        if (arr[i] != 0)
-        {
-            cout << arr[i] << '\n';
-        }
-    }
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int M,N;
+	cin>>M>>N;
+	
+	for(int i=M;i<=N;i++)
+	{
+		int sum = 0;
+		for(int j=2; j<=sqrt(i); j++)
+		{
+			if(i % j == 0)
+			{
+				sum += 1;
+				break;
+			}
+		    else{
+		    	continue;
+		    }
+			
+		}
+		if(sum == 0 && i != 1)
+		{
+			cout<<i<<"\n";
+		}
+		else{
+			continue;
+		}
+	}
+	
+	return 0;
 }
