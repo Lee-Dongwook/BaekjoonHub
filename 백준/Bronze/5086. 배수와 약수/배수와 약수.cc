113f8dfbfae7febcf99bb1c49670b35b789d3b99
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-02 23:26:22 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -3,28 +3,32 @@ using namespace std;
 
 int main(void)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	while (1)
-	{
-		int a, b;
-		cin >> a >> b;
-		if (a == 0 && b == 0)
-		{
-			return 0;
-		}
-		if (b % a == 0 && b >= a)
-		{
-			cout << "factor" << "\n";
-		}
-		else if (a % b == 0 && a >= b)
-		{
-			cout << "multiple" << "\n";
-		}
-		else {
-			cout << "neither" << "\n";
-		}
-	}
+   while(1)
+   {
+       int a, b;
+       cin>>a>>b;
+       
+       if(a == 0 && b == 0)
+       {
+           break;
+       }
+       
+       else{
+           
+           if(a < b  && b % a == 0)
+           {
+               cout<<"factor"<<"\n";
+           }
+           else if(a % b == 0 && b < a)
+           {
+               cout<<"multiple"<<"\n";
+           }
+           else {
+               cout<<"neither"<<"\n";
+           }
+           
+       }
+   }
+   
+   return 0;
 }
