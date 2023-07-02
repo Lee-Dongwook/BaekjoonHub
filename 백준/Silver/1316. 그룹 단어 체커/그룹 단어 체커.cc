8651ac1c06d06abd37dd78464fb4091419190717
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-02 14:41:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -1,44 +1,58 @@
 #include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
-int main() { 
-	int N;                                // 입력받을 단어의 개수
-	string word;                          // 입력받은 단어
-	int count = 0;                        // 그룹 단어가 아니라면 카운트
+map<char,int> m;
 
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> word;
-        
-		// 단어에서 알파벳 문자의 출현유무를 나타내는 배열 (출현없으면 false)
-		bool alphabet[26] = { false, };        
-		alphabet[(int)(word[0]) - 97] = true;  // 첫번째 단어값을 true로 설정
-
-		for (int i = 1; i < word.size(); i++) {
-        
-			// 1. i번째 문자가 i-1번째 문자와 같으면 연속이므로 넘어간다.
-			if (word[i] == word[i - 1]) {
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	cin>>N;
+	
+	int sum = 0;
+	
+	while(N--)
+	{
+		string str;
+		cin>>str;
+		
+		bool a = true;
+		
+		m.insert({str[0],1});
+		for(int i=1;i<str.size();i++)
+		{
+			if(str[i] == str[i-1])
+			{
 				continue;
 			}
-            
-			// 2. i번째 문자가 i-1번째 문자와 같지 않고, (연속하지 않고)
-			//    해당 배열값이 true라면 (이미 나왔던 문자라면)
-			else if(word[i] != word[i - 1] 
-            		&& alphabet[(int)(word[i]) - 97] == true){
-				count++;	// 그룹단어가 아니므로 카운트
-				break;
-			}
-            
-			// 3. 위의 두 경우에 해당하지 않는 경우
-			//    처음 등장한 문자인 경우
 			else {
-				alphabet[(int)(word[i]) - 97] = true;
+				auto iter = m.find(str[i]);
+				if(iter != m.end())
+				{
+					a = false;
+					break;
+				}
+				else{
+					m.insert({str[i],1});
+				}
 			}
 		}
+		
+		if(a == true){
+			sum += 1;
+		}
+		else if(a == false){
+			sum += 0;
+		}
+		
+		m.erase(m.begin(), m.end());
 	}
-
-	// 그룹 단어의 개수 = 전체단어의 개수 - 그룹단어가 아닌 단어의 개수
-	cout << N - count;
-
+	
+	cout<<sum;
+	
 	return 0;
 }
