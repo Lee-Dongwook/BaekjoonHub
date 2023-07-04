9f299fa8d57322e021b5c541e180cde087640c0e
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-04 14:43:39 +0900

    [Silver II] Title: 좌표 압축, Time: 1312 ms, Memory: 39912 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,30 +1,55 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
+
+vector<int> pre;
+vector<int> vec;
+map<int, int> m;
 
 int main(void)
 {
-	int n;
-	long long temp;
-	vector <long long> x1;
-	vector <long long> x2;
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> temp;
-		x1.push_back(temp); //정렬 벡터
-		x2.push_back(temp); //원본 벡터
-	}
-
-	sort(x1.begin(), x1.end()); //정렬
-	x1.erase(unique(x1.begin(), x1.end()), x1.end()); // 중복값 제거
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << lower_bound(x1.begin(), x1.end(), x2[i]) - x1.begin() << " ";
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N;
+    cin>>N;
+    
+    while(N--)
+    {
+        int num;
+        cin>>num;
+        pre.push_back(num);
+        vec.push_back(num);
+    }
+    
+    sort(vec.begin(),vec.end());
+    
+    static int a = 0;
+    
+    for(int i=0;i<vec.size();i++)
+    {
+        auto iter = m.find(vec[i]);
+        if(iter != m.end())
+        {
+            continue;
+        }
+        else{
+            m.insert({vec[i],a});
+            a++;
+        }
+    }
+    
+    for(int i=0;i<pre.size();i++)
+    {
+        auto iter = m.find(pre[i]);
+        if(iter != m.end())
+        {
+            cout<<iter->second<<" ";
+        }
+    }
+    
+    return 0;
 }
