fec8e452dd27def8e5d0b95a49b2f5243c642b40
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-07 14:35:07 +0900

    [Silver V] Title: 숫자 카드, Time: 468 ms, Memory: 28368 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,44 +1,49 @@
 #include <iostream>
-#include <algorithm>
+#include <set>
+#include <vector>
 using namespace std;
 
-int a[500001];
-int b[500001];
-int c[500001]= {0,};
+set<int> s;
+vector<int> vec;
 
-int main(void)
-{ int N;
-  cin>>N;
-  for(int i=0; i<N; i++)cin>>a[i];
-  
-  int M;
-  cin>>M;
-  for(int i=0;i<M;i++)cin>>b[i];
- 
-  sort(a,a+N);
-  for(int i=0;i<M;i++)
-  { int right = N-1;
-    int left = 0;
-    while(left<=right)
-    {    int mid = (right+left)/2;
-         if(a[mid] == b[i])
-         {
-             c[i] = 1;
-             break;
-         }
-         else if(a[mid] > b[i])
-         {
-             right = mid - 1;
-         }
-         else
-         {
-             left = mid + 1;
-         }
-            
-    }
-  }
-
-for(int i = 0; i < M ; i++) cout << c[i]<<" ";
- return 0;
-
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	cin>>N;
+	
+	while(N--)
+	{
+		int num;
+		cin>>num;
+		s.insert(num);
+	}
+	
+	int M;
+	cin>>M;
+	
+	while(M--)
+	{
+		int num;
+		cin>>num;
+		
+		auto iter = s.find(num);
+		if(iter != s.end())
+		{
+			vec.push_back(1);
+		}
+		else{
+			vec.push_back(0);
+		}
+	}
+	
+	for(int i=0;i<vec.size(); i++)
+	{
+		cout<<vec[i]<<" ";
+	}
+	
+	return 0;
 }
