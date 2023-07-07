8d48a9315e29ebc3f86e3cd57b7b3a8e3b4de64c
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-07 14:51:07 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 916 ms, Memory: 399432 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -1,21 +1,47 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <set>
+#include <algorithm>
 using namespace std;
- 
+
+vector<string> vec;
+set<string> s;
+
 int main() {
-    string s;
-    cin >> s;
- 
-    set<string> set;
- 
-    string str = "";
-    for (int i = 0; i < s.size(); i++) {
-        for (int j = i; j < s.size(); j++) {
-            str += s[j];
-            set.insert(str);
-        }
-        str = "";
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	string word;
+	cin>>word;
+	
+	for(int i=0;i<word.size();i++)
+	{
+		string temp ="";
+		for(int j = i; j<word.size();j++)
+		{
+			temp += word[j];
+			
+			auto iter = s.find(temp);
+			if(iter != s.end())
+			{
+				continue;
+			}
+			else{
+			   s.insert(temp);	
+			}
+		}
+	}
+   
+    for(auto iter = s.begin(); iter != s.end(); iter++)
+    {
+    	vec.push_back(*iter);
     }
- 
-    cout << set.size();
+    
+    cout<<vec.size();
+
+	
+	return 0;
 }
