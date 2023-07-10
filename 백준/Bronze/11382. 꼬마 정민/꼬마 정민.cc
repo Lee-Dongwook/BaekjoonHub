781406058d98d6260bfe2f0d6599df4bfa0fcf6a
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-11 01:09:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    long long a,b,c;
+    cin>>a>>b>>c;
+    
+    long long result = a + b + c;
+    cout<<result;
+    return 0;
+}
