f27f7cb99af388e78bac706173ffce9d480b0d41
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-11 01:13:53 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if(n==0){
+        return 0;
+    }
+    else if(n == 1)
+    {
+        return 1;
+    }
+    else{
+        return fib(n-1) + fib(n-2);
+    }
+   
+}
+
+int main(void)
+{
+    int a;
+    cin>>a;
+    
+    int res = fib(a);
+    cout<<res;
+}
