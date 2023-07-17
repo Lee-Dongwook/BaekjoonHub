78dcc491b7717441980a28823d76def41d6572ba
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-17 21:17:14 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,43 +1,39 @@
-
-#include<iostream>
-#include<queue>
-
+#include <iostream>
+#include <queue>
 using namespace std;
 
-int N;
-
-priority_queue<int, vector<int>, greater<int>> p_queue;
+priority_queue<int> pq;
 
 int main() {
-
+	
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N;
-
-	for (int i = 0; i < N; i++) {
-		int temp;
-		cin >> temp;
-
-		if (temp == 0) {
-			if (p_queue.empty()) {
-				cout << 0 << "\n";
-			}
-			else {
-				cout << p_queue.top() << "\n";
-				p_queue.pop();
-			}
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	cin>>N;
+	
+	while(N--)
+	{
+		int num;
+		cin>>num;
+		
+		if(num > 0)
+		{
+			pq.push(-num);
 		}
-		else {
-			p_queue.push(temp);
+		else if(num == 0)
+		{
+			if(pq.empty())
+			{
+				cout<<0<<"\n";
+			}
+			else{
+			  cout<<-pq.top()<<"\n";
+			  pq.pop();
+			}
 		}
 	}
-
+	
 	return 0;
-
 }
- 
-
-
-
