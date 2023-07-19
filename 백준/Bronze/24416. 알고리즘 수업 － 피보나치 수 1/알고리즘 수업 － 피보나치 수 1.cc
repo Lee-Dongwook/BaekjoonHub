18cc14a41687a9af0e11c657425ee3ec5aa6039a
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-19 21:27:58 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
@@ -1,28 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int arr[41] = {0};
-int n, cnt1 = 0, cnt2 = 0;
+int a = 0;
+int b = 0;
+int DP[41];
 
-int fib(int N) {
-    if (N == 1 || N == 2) {
-      cnt1++;
-      return 1;  
-    }
-    else return (fib(N - 1) + fib(N - 2));
+void init()
+{
+	for(int i = 0; i<41;i++)
+	{
+		DP[i] = 0;
+	}
 }
 
-int main(){
-  
-  cin>>n;
-  
-  fib(n);
-  
-  arr[1] = arr[2] = 1;
-  for(int i=3;i<=n;i++){
-    arr[i] = arr[i-1] + arr[i-2];
-    cnt2++;
-  }
-  cout<<cnt1<<" "<<cnt2;
-  return 0;
+int fib(int n)
+{
+	if(n == 1 || n == 2)
+	{
+		a += 1;
+		return 1;
+	}
+	else
+	  return fib(n-1) + fib(n-2);
+}
+
+int dp_fib(int n)
+{
+	DP[1] = 1;
+	DP[2] = 1;
+	
+	for(int i=3;i<=n;i++)
+	{
+		DP[i] = DP[i-1] + DP[i-2];
+		b += 1;
+	}
+	return DP[n];
+}
+
+int main() {
+	
+	init();
+	int n;
+	cin>>n;
+	fib(n);
+	dp_fib(n);
+	
+	cout<<a<<" "<<b;
+		
+	return 0;
 }
