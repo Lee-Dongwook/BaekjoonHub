eab20617655f4eb9865e4c1f1ca318f3d2ef4b26
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-07-19 22:45:53 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-long long num[1000001];
+int N;
+int DP[1000001];
 
+void init(int N)
+{
+	DP[0] = 0;
+	DP[1] = 1;
+	DP[2] = 2;
+	for(int i=3;i<=N;i++)
+	{
+		DP[i] = (DP[i-1] % 15746 + DP[i-2] % 15746) % 15746;
+	}
+}
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int N;
-	cin >> N;
-
-	num[0] = 0;
-	num[1] = 1;
-	num[2] = 2;
-	for (int i = 3; i <= N; i++)
-	{
-		num[i] = num[i - 1] + num[i - 2];
-		num[i] = num[i] % 15746;
-	}
-
-	cout << num[N] % 15746;
-
-
-	return 0;
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin>>N;
+	init(N);
+	cout<<DP[N];
+	
+	
+	return 0; 
 }
