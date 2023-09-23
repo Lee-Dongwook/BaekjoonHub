a63f7276df3fe3718bdf86f60dc19331a5480f35
LeeDongWook <97590636+Lee-Dongwook@users.noreply.github.com>
2023-09-23 21:04:05 +0900

    [Silver I] Title: 스타트링크, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.cc
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -1,67 +1,55 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define MAX 1000001
 using namespace std;
 
 int F, S, G, U, D;
 
-bool visited[MAX];
-int path[MAX];
-vector<int> dx;
-queue<int> q;
+int bfs() {
+    vector<bool> visited(F + 1, false);
+    queue<pair<int, int>> q;  // 층과 버튼 클릭 횟수를 저장하는 큐
+    q.push({S, 0});  // 시작 층과 버튼 클릭 횟수 0으로 초기화
+    visited[S] = true;
 
-void bfs(int x)
-{
-	visited[x] = true;
-	q.push(x);
-	while (!q.empty())
-	{
-		int cur = q.front();
-		q.pop();
+    while (!q.empty()) {
+        int current_floor = q.front().first;
+        int clicks = q.front().second;
+        q.pop();
 
-		for (int i = 0; i < 2; i++)
-		{
-			int nx = cur + dx[i];
+        if (current_floor == G) {
+            return clicks;  // G층에 도착했을 때 버튼 클릭 횟수 반환
+        }
 
-			if (0 < nx && nx <= F)
-			{
-				if (!visited[nx])
-				{
-					visited[nx] = true;
-					q.push(nx);
-					path[nx] = path[cur] + 1;
-				}
-			}
-		}
+        // U 버튼을 누른 경우
+        if (current_floor + U <= F && !visited[current_floor + U]) {
+            visited[current_floor + U] = true;
+            q.push({current_floor + U, clicks + 1});
+        }
 
-		
-	}
+        // D 버튼을 누른 경우
+        if (current_floor - D >= 1 && !visited[current_floor - D]) {
+            visited[current_floor - D] = true;
+            q.push({current_floor - D, clicks + 1});
+        }
+    }
+
+    return -1;  // G층에 도착할 수 없는 경우
 }
 
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-int main(void)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin >> F >> S >> G >> U >> D;
 
+    int result = bfs();
 
-	cin >> F >> S >> G >> U >> D;
+    if (result == -1) {
+        cout << "use the stairs";
+    } else {
+        cout << result;
+    }
 
-	dx.push_back(U);
-	dx.push_back(D*-1);
-
-	bfs(S);
-
-	if (visited[G])
-	{
-		cout << path[G];
-	}
-	else {
-		cout << "use the stairs";
-	}
-
-	return 0;
-
+    return 0;
 }
