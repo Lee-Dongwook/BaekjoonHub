6c562c9b1a7d02c3c7b53ba7a31a92769dc7a364
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-01-13 15:20:14 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,54 +1,50 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
 #include <vector>
+#define MAX 101
 using namespace std;
 
 int N, M;
-vector<int>adjList[101];
-bool isvisited[101] = { 0 , };
-stack<int>st;
+int cnt = 0;
+vector<int> adjList[MAX];
+bool isvisited[MAX];
 
-int DFS(int V)
+void DFS(int start)
 {
-	int cnt = 0;
-	st.push(V);
-	while (!st.empty())
-	{
-		int cur = st.top();
-		st.pop();
-
-		if(isvisited[cur] == true) { continue; }
-		isvisited[cur] = true;
-		
-		if(cur != 1)
-		  cnt++;
-
-		for (int i = adjList[cur].size() - 1; i >= 0; i--)
-		{
-			int next = adjList[cur][i];
-			st.push(next);
-		}
-
-	}
-	return cnt;
+    isvisited[start] = true;
+    cnt += 1;
+    
+    for(int i = 0; i < adjList[start].size(); i++)
+    {
+        int next = adjList[start][i];
+        
+        if(isvisited[next] == false)
+        {
+            DFS(next);
+        }
+    }
 }
+
 
 int main(void)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> N >> M;
-	while (M--)
-	{
-		int u, v;
-		cin >> u >> v;
-		adjList[u].push_back(v);
-		adjList[v].push_back(u);
-	}
-	int result = DFS(1);
-	cout << result;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N >> M;
+    
+    for(int i = 0; i < M; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        
+        adjList[start].push_back(end);
+        adjList[end].push_back(start);
+    }
+    
+    DFS(1);
+    
+    cout << cnt - 1;
+    
+    return 0;
 }
