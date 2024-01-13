2b3e1e8c8426f87711dd490257defa6ce2382e1c
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-01-13 15:54:42 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,82 +1,72 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define INF 987654321
+#define MAX_INT 987654321
 using namespace std;
 
-int V,E;
-int K;
+int V, E, K;
 
-vector<pair<int, int>> vec[20001];
-int D[20001];
-priority_queue<pair<int,int>>pq;
+vector<pair<int,int>> adjList[20001];
+int dist[20001];
+priority_queue<pair<int,int>> pq;
 
-void init()
+void Dijkstra(int start)
 {
-	for(int i=0;i<20001;i++)
-	{
-		D[i] = INF;
-	}
+    pq.push({0, start});
+    dist[start] = 0;
+    
+    while(!pq.empty())
+    {
+        int current_cost = -pq.top().first;
+        int current_position = pq.top().second;
+        pq.pop();
+        
+        if(dist[current_position] < current_cost) { continue; }
+        
+        for(int i = 0; i < adjList[current_position].size(); i++) 
+        {
+            int next_cost = adjList[current_position][i].first;
+            int next_position = adjList[current_position][i].second;
+            
+            if(dist[next_position] > next_cost + current_cost)
+            {
+                dist[next_position] = next_cost + current_cost;
+                pq.push({-dist[next_position], next_position});
+            }
+        }
+        
+    }
+    
 }
 
-void Dijikstra(int start)
+int main(void)
 {
-	pq.push({0,start});
-	D[start] = 0;
-	while(!pq.empty())
-	{
-		int cur_cost = -pq.top().first; 
-		int cur_x = pq.top().second;
-		pq.pop();
-		
-		if(D[cur_x] < cur_cost)
-		{
-			continue;
-		}
-		
-		for(int i=0;i<vec[cur_x].size();i++)
-		{
-			int next_x = vec[cur_x][i].second;
-			int next_cost = vec[cur_x][i].first;
-			if(D[next_x] > cur_cost + next_cost)
-			{
-				D[next_x] = cur_cost + next_cost;
-				pq.push({-D[next_x],next_x});
-			}
-		}
-		
-	}
-}
-
-int	main() {
-	
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	
-	cin>>V>>E;
-	cin>>K;
-	
-	while(E--)
-	{
-		int start,end,cost;
-		cin>>start>>end>>cost;
-		
-		vec[start].push_back({cost,end});
-	}
-	
-	init();
-	Dijikstra(K);
-	for(int i=1;i<=V;i++)
-	{
-		if(D[i] == INF)
-		{
-			cout<<"INF"<<"\n";
-		}
-		else{
-			cout<<D[i]<<"\n";
-		}
-	}
-	
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> V >> E >> K;
+    fill(dist, dist + V + 1, MAX_INT);
+    
+    for(int i = 0; i < E; i++)
+    {
+        int start, end, cost;
+        cin >> start >> end >> cost;
+        
+        adjList[start].push_back(make_pair(cost, end));
+    }
+    
+    Dijkstra(K);
+    
+    for(int i = 1; i <= V; i++)
+    {
+        if(dist[i] == MAX_INT)
+        {
+            cout <<"INF"<< "\n";
+        } else {
+            cout << dist[i] << "\n";
+        }
+    }
+    
+    return 0;
 }
