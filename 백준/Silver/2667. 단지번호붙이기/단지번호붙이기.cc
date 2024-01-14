82e440e80a498d20ecc8645279bd84007b3d2a4e
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-01-14 20:37:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -5,88 +5,80 @@
 #include <algorithm>
 using namespace std;
 
-int map[25][25]; //지도
-int N; //지도 크기
-bool isvisited[25][25] = {false}; //방문 여부
-int dx[4] = {-1,1,0,0};//x좌표 이동
-int dy[4] = {0,0,-1,1};//y좌표 이동
-vector<int> vec; //단지 수 저장
-queue<pair<int,int>>q; //단지 저장
+int N;
+int map[25][25];
+bool isvisited[25][25] = {false, };
+queue<pair<int,int>> q;
+vector<int> answer;
 
-void BFS(int x, int y)
-{
-    int cnt = 0; //단지 수 카운트
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+void BFS(int x, int y) {
+    int cnt = 0;
     q.push({x,y});
     isvisited[x][y] = true;
-    
-    while(!q.empty())
-    {
+
+    while(!q.empty()) {
         int cur_x = q.front().first;
         int cur_y = q.front().second;
-        cnt++;
+
+        cnt += 1;
+
         q.pop();
-        
-        for(int i=0;i<4;i++)
-        {
+
+        for(int i = 0; i < 4; i++){
             int next_x = cur_x + dx[i];
             int next_y = cur_y + dy[i];
-            
-            if(next_x >=0 && next_x < N && next_y >=0 && next_y <N)
-            {
-                if(map[next_x][next_y] == 1 && !isvisited[next_x][next_y])
-                {
-                    
-                    q.push({next_x,next_y});
-                    isvisited[next_x][next_y] = true;
-                }
-                else{
-                	continue;
-                }
+
+            if(next_x < 0 || next_x >= N || next_y < 0 || next_y >= N){ continue; }
+
+            if(map[next_x][next_y] == 1 && isvisited[next_x][next_y] == false) {
+                q.push({next_x, next_y});
+                isvisited[next_x][next_y] = true;
             }
         }
-        
     }
-    
-   vec.push_back(cnt);
+
+    if(cnt > 0) {
+        answer.push_back(cnt);
+    }
 }
 
-
-int main()
-{
+int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    
-    cin>>N;
-    
-    for(int i=0;i<N;i++)
-    {
-    	string s;
-    	cin>>s;
-    	
-    	for(int j=0;j<s.size();j++)
-    	{
-    		map[i][j] = s[j]-'0';
-    	}
-    }
-    
-    for(int i=0;i<N;i++)
-    {
-        for(int j=0;j<N;j++)
-        {
-            if(map[i][j] == 1 && isvisited[i][j] == false)
-            {
-            	 BFS(i,j);
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        string numList;
+        cin >> numList;
+
+        for(int j = 0; j < numList.length(); j++) {
+            if(numList[j] == '1') {
+                map[i][j] = 1;
+            } else {
+                map[i][j] = 0;
             }
         }
     }
-    
-    sort(vec.begin(),vec.end());
-    
-    cout<<vec.size()<<"\n";
-    for(int i=0;i<vec.size();i++)
-    {
-        cout<<vec[i]<<"\n";
+
+    for(int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++){
+            if(isvisited[i][j] == false && map[i][j] == 1) {
+                BFS(i,j);
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+
+    cout << answer.size() << "\n";
+
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << "\n";
     }
 
     return 0;
