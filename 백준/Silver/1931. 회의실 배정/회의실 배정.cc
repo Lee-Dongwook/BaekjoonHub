fe70d793c67b9c69bc3a012f7b3214f0d83590fa
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-01-15 13:16:39 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,40 +1,52 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+int N;
+int ans = 0;
+
+vector<pair<int,int>> vec;
+
+bool cmp (pair<int,int> a, pair<int,int> b)
+{
+    if(a.second == b.second) {
+        return a.first < b.first;
+    }
+    
+    return a.second < b.second;
+}
 
 int main(void)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int N;
-	cin >> N;
-
-	vector<pair<int, int>>A;
-	A.resize(N);
-
-	for (int i = 0; i < N; i++)
-	{
-		cin >> A[i].second;
-		cin >> A[i].first;
-	}
-
-	sort(A.begin(), A.end());
-
-	int cnt = 0;
-	int end = -1;
-
-	for(int i=0;i<N;i++)
-	{
-		if (A[i].second >= end)
-		{
-			end = A[i].first;
-			cnt++;
-		}
-	}
-
-	cout << cnt << "\n";
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> N;
+    
+    for(int i = 0; i < N; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        
+        vec.push_back(make_pair(start, end));
+    }
+    
+    sort(vec.begin(), vec.end(), cmp);
+    
+    int endTime = vec[0].second;
+    ans += 1;
+    
+    for(int i = 1; i <= N ; i++) {
+      if(vec[i].first >= endTime) {
+          ans += 1;
+          endTime = vec[i].second;
+      }
+    }
+    
+   
+    cout << ans;
+   
+    return 0;
 }
