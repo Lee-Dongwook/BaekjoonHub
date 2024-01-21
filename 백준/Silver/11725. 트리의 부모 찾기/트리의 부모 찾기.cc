3b5e0be275c599f40701b1baa225e38176927db4
이동욱 <97590636+Lee-Dongwook@users.noreply.github.com>
2024-01-21 17:27:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 48 ms, Memory: 12488 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,36 +1,43 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
 #define MAX 100001
+using namespace std;
 
 int N;
-int arr[MAX];
-bool visited[MAX];
-vector<int> v[MAX];
+vector<int> Tree[MAX];
+int parent[MAX];
 
-void dfs(int k) {
-    visited[k]=true;
-    for(int i=0;i<v[k].size();i++) {
-        int next = v[k][i];
-        if(!visited[next]) {
-            arr[next]=k;
-            dfs(next);
+void dfs(int current, int p) {
+    parent[current] = p;
+    
+    for(int i = 0; i < Tree[current].size(); i++) {
+        int next = Tree[current][i];
+        if(next != p) {
+            dfs(next, current);   
         }
     }
 }
 
-int main() {
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     cin >> N;
-
-    for(int i=0;i<N;i++) {
-        int x,y;
-        cin >> x >> y;
-        v[x].push_back(y);
-        v[y].push_back(x);
+    
+    for(int i = 0; i < N -1; i++) {
+        int start, end;
+        cin >> start >> end;
+        
+        Tree[start].push_back(end);
+        Tree[end].push_back(start);
     }
-
-    dfs(1);
-
-    for(int i=2;i<=N;i++) cout << arr[i] << "\n";
+    
+    dfs(1,0);
+    
+    for (int i = 2; i <= N; i++) {
+        cout << parent[i] << "\n";
+    }
+    
+    return 0;
 }
